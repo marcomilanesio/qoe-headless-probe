19bd6049b1b473626427bb2bf497c6098b6ee003
Marco Milanesio <milanesio.marco@gmail.com>
2016-03-01 23:20:57 +0100

    concurrent.future in activemeasurements
    Signed-off-by: Marco Milanesio <milanesio.marco@gmail.com>

--- probe/activemeasure.py
+++ probe/activemeasure.py
@@ -18,16 +18,16 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import subprocess
+#import subprocess
 import json
-import re
+#import re
 import logging
-import numpy
-import os
+#import numpy
+#import os
 
 logger = logging.getLogger('Active')
 
-
+'''
 class Measure(object):
     def __init__(self, host):
         self.target = host
@@ -171,6 +171,85 @@ class TracerouteHop(object):
 
     def __str__(self):
         return '%d: %s, %.3f %s' % (self.hop_nr, self.ip_addr, self.rtt['avg'], str(self.endpoints))
+'''
+
+import concurrent.futures
+import time
+import subprocess
+import re
+import numpy
+
+
+def run_traceroute(dest, ttl=32):
+    trace = []
+    HEADER_REGEXP = re.compile(r'traceroute to (\S+) \((\d+\.\d+\.\d+\.\d+)\)')
+    IPADDR_REGEXP = re.compile(r'\d+\.\d+\.\d+\.\d+')
+    RTT_REGEXP = re.compile(r'\d+.\d+')
+
+    rtt = {'min': 0.0, 'max': 0.0, 'avg': 0.0, 'std': 0.0}
+
+    cmd = "traceroute -n -m {0} {1}".format(ttl, dest)
+    out, err = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+    if err:
+        print("Error: ", err)
+    for line in out.decode("utf-8").split("\n"):
+        if HEADER_REGEXP.match(line):  # header
+            continue
+        else:
+            hop = [x for x in line.split(" ") if x is not '']
+            if len(hop) == 0:
+                continue
+            else:
+                hop_nr = int(hop[0])
+                ip_addr = hop[1] if IPADDR_REGEXP.match(hop[1]) else 'n.a.'
+                if ip_addr == 'n.a.':
+                    for k in rtt.keys():
+                        rtt[k] = -1
+                else:
+                    latencies = list(map(float, [x for x in hop[2:] if RTT_REGEXP.match(x) and not IPADDR_REGEXP.match(x)]))
+                    rtt['min'] = min(latencies)
+                    rtt['max'] = max(latencies)
+                    rtt['avg'] = numpy.mean(latencies)
+                    rtt['std'] = numpy.std(latencies)
+            trace.append({'hop_nr': hop_nr, 'ip_addr': ip_addr, 'rtt': rtt})
+    return trace
+
+
+def get_match_groups(ping_output, regex):
+    match = regex.search(ping_output)
+    if not match:
+        raise Exception('Invalid PING output:\n' + ping_output)
+    return match.groups()
+
+
+def parse(ping_output):
+    po = ping_output.decode("utf-8")
+    matcher = re.compile(r'PING ([a-zA-Z0-9.\-]+) \(')
+    host = get_match_groups(po, matcher)[0]
+    matcher = re.compile(r'(\d+) packets transmitted, (\d+) received, (\d+)% packet loss, ')
+    try:
+        sent, received, loss = map(int, get_match_groups(po, matcher))
+    except Exception as e:
+        logger.error(e)
+        return {'host': host, 'sent': -1, 'received': -1, 'loss': -1, 'min': -1,
+            'avg': -1, 'max': -1, 'std': -1}
+
+    try:
+        matcher = re.compile(r'(\d+.\d+)/(\d+.\d+)/(\d+.\d+)/(\d+.\d+)')
+        rttmin, rttavg, rttmax, rttmdev = map(float, get_match_groups(po, matcher))
+    except:
+        rttmin, rttavg, rttmax, rttmdev = [-1]*4
+
+    return {'host': host, 'sent': sent, 'received': received, 'loss': loss, 'min': rttmin,
+            'avg': rttavg, 'max': rttmax, 'std': rttmdev}
+
+
+def run_ping(dest):
+    ping = subprocess.Popen("ping -c 5 {}".format(dest), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, error = ping.communicate()
+    if error:
+        return "Error"
+    return parse(out)
 
 
 class ActiveMonitor(object):
@@ -201,18 +280,33 @@ class ActiveMonitor(object):
 
             if server_ip not in probed_ip.keys():
                 logger.debug("Running ping for {0}".format(server_ip))
-                ping = Ping(server_ip)
-                ping.run()
-                ping_result = ping.get_result()
+                ping = run_ping(server_ip)
                 logger.debug("Running traceroute for {0}".format(server_ip))
-                trace = Traceroute(server_ip)
-                trace.run()
-                trace_result = trace.get_result()
+                trace = run_traceroute(server_ip)
+
+                # first three hops
+                addr = [x['ip_addr'] for x in trace if x['hop_nr'] < 4]
+
+                with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+                    for ip, res in zip(addr, executor.map(run_ping, addr)):
+                        p_result = json.dumps(res)
+                        probed_ip[ip] = {'ping': p_result}
+                        to_insert.append({'url': url, 'ip': ip, 'ping': p_result})
+
+                ping_result = json.dumps(ping)
+                trace_result = json.dumps(trace)
+                #ping = Ping(server_ip)
+                #ping.run()
+                #ping_result = ping.get_result()
+                #logger.debug("Running traceroute for {0}".format(server_ip))
+                #trace = Traceroute(server_ip)
+                #trace.run()
+                #trace_result = trace.get_result()
 
                 to_insert.append({'url': url, 'ip': server_ip, 'ping': ping_result, 'trace': trace_result})
                 #tot[sid].append({'url': url, 'ip': server_ip, 'ping': ping_result, 'trace': trace_result})
                 probed_ip[server_ip] = {'ping': ping_result, 'trace': trace_result}
-
+                '''
                 for k, v in trace.get_first_hops().items():
                     if v != 'n.a.':
                         p = Ping(v)
@@ -222,7 +316,7 @@ class ActiveMonitor(object):
                         to_insert.append({'url': url, 'ip': v, 'ping': p_result})
                     else:
                         logger.warning("Hop {0} [{1}] not answering to ping".format(k, v))
-
+                '''
             else:
                 to_insert.append({'url': url, 'ip': server_ip, 'ping': probed_ip[server_ip]['ping'],
                                   'trace': probed_ip[server_ip]['trace']})
@@ -245,10 +339,9 @@ class ActiveMonitor(object):
         done = {}
         for ip in ip_addrs:
             if ip not in probed_ip.keys():
-                ping = Ping(ip)
                 logger.debug("Running ping for ip : {0}".format(ip))
-                ping.run()
-                res.append({'url': url, 'ip': ip, 'ping': ping.get_result()})
+                ping = run_ping(ip)
+                res.append({'url': url, 'ip': ip, 'ping': json.dumps(ping)})
                 done[ip] = {'ping': ping}
             else:
                 res.append({'url': url, 'ip': ip, 'ping': probed_ip[ip]['ping']})
